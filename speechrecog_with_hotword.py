94b0b4911f2afb81d5e62b65193448ace8b2e044
Shashank <s.p.rao@student.tudelft.nl>
2018-04-08 12:23:35 +0200

    First commit to befriend Dobby

---
+++ speechrecog_with_hotword.py
@@ -0,0 +1,37 @@
+import os
+import speech_recognition as sr
+
+r = sr.Recognizer()
+m = sr.Microphone()
+
+ROOT = os.getcwd()
+snowboy_location = os.path.join(ROOT, "snowboy")
+snowboy_hotword = ["Dobby_S.pmdl"]
+snowboy_params = (snowboy_location, snowboy_hotword)
+def speech_text():
+
+    try:
+        print("A moment of silence, please...")
+        with m as source: r.adjust_for_ambient_noise(source)
+        print("Set minimum energy threshold to {}".format(r.energy_threshold))
+        while True:
+            print("Say something!")
+            with m as source: audio = r.listen(source, snowboy_configuration = snowboy_params)
+            print("Got it! Now to recognize it...")
+            try:
+                # recognize speech using Google Speech Recognition
+                value = r.recognize_google(audio)
+
+                # we need some special handling here to correctly print unicode characters to standard output
+                if str is bytes:  # this version of Python uses bytes for strings (Python 2)
+                    print(u"You said {}".format(value).encode("utf-8"))
+                else:  # this version of Python uses unicode for strings (Python 3+)
+                    print("You said {}".format(value))
+            except sr.UnknownValueError:
+                print("Oops! Didn't catch that")
+            except sr.RequestError as e:
+                print("Uh oh! Couldn't request results from Google Speech Recognition service; {0}".format(e))
+    except KeyboardInterrupt:
+        pass
+
+speech_text()
