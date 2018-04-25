4f960518612a4807b1de4128e4b80b0136e81a38
Shashank <s.p.rao@student.tudelft.nl>
2018-04-25 14:24:10 +0200

    trying multiprocessing to animate leds

--- led_rotate.py
+++ led_rotate.py
@@ -4,25 +4,17 @@ import time
 
 def set_leds():
     image = everloop.Image()
-    image.leds[0].red = 20
+    image.leds[0].blue = 20
     cnt = 1
     for i in range(1, len(image.leds)):
         if i <= everloop.EVERLOOP_SIZE // 2:
-            image.leds[i].red = 17
-            image.leds[i].blue = cnt // 5
+            image.leds[i].blue = 17
+            image.leds[i].red = cnt // 5
             cnt += 1
         else:
-            image.leds[i].red = 17
+            image.leds[i].blue = 17
             image.leds[i].green = cnt // 5
             cnt -= 1
-    return image
-
-def rotate_leds(start_rotation=True):
-    while start_rotation:
-        image = set_leds()
-        image.render()
-        image.rotate(1)
-        time.sleep(0.03)
-
-# if __name__ == '__main__':
-#     rotate_leds()
+    image.render()
+    image.rotate(1)
+    time.sleep(0.03)
